bb11c40c0506e9c2a20ba496ed42ec43563f7360
golovachevavp <golovacheva.vp@gmail.com>
2022-09-17 21:21:18 +0300

    1

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,26 +1,49 @@
 public class ArrayExample {
     public static void main(String[] args) {
-        double[] numbers = new double[5];
-        double max = 0, averageValue = 0, sum = 0;
-        for (int i = 0; i < numbers.length ; i++) {
+
+        double[] numbers = arrayNumbers(5);
+        System.out.println("Минимальное значение : " + minValue(numbers));
+        System.out.println("Максимальное значение: " + maxValue(numbers));
+        System.out.println("Среднее значение: " + averageValue(numbers));
+    }
+
+    public static double[] arrayNumbers(int n){
+        double[] numbers = new double[n];
+        for (int i = 0; i < numbers.length; i++) {
             numbers[i] = Math.random();
-            sum += numbers[i];
             System.out.println(numbers[i]);
         }
+        return numbers;
+    }
+
+    public static double minValue(double[] numbers) {
         double min = numbers[0];
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] < min) {
                 min = numbers[i];
             }
+        }
+        return min;
+    }
+
+    public static double maxValue(double[] numbers) {
+        double max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] > max) {
                 max = numbers[i];
             }
         }
-        averageValue = sum / numbers.length;
-        System.out.println("Минимальное значение : " + min);
-        System.out.println("Максимальное значение: " + max);
-        System.out.println("Среднее значение: " + averageValue);
+        return max;
     }
 
-}
+    public static double averageValue(double[] numbers) {
+        double sum=0, averageValue=0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum+=numbers[i];
+        }
+        averageValue = sum / numbers.length;
+        return  averageValue;
+    }
 
+
+}
